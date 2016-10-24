d667b93e8fb00d4759c8479550be6eaa44322c07
Tatiana Roga <you@example.com>
2016-10-24 12:16:08 +1000

    asdfadf

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
 
  module.exports = {
-  'url' : 'mongodb://egghead:Seventy3#limes@ds031167.mlab.com:31167/heroku_s3jmfw6n
-'
-}
+  'url' : 'mongodb://heroku_s3jmfw6n:8rvdqm3e174pk7dp89rkk2sj05@ds031167.mlab.com:31167/heroku_s3jmfw6n'
+};
