b3f2aa9e409f432f3007d6429a3aac824245b448
Tatiana Roga <you@example.com>
2016-12-06 15:45:06 +1000

    signup validation

--- public/js/validation.js
+++ public/js/validation.js
@@ -3,6 +3,7 @@ var validateUsername = function(_username) {
 	if (re.test(document.getElementById(_username).value)){
 		return true;
 	} else {
+		$('#usernameError').empty();
 		$('#usernameError').append('<p>Please stick to letters, hyphens and apostrophes</p>');
 		return false; 
 	}
@@ -14,6 +15,7 @@ var validateEmail = function(_email) {
 		return true;
 	}
 	else {
+		$('#emailError').empty();
 		$('#emailError').append('<p>Please enter a valid email address</p>');
 		return false; 
 	}
@@ -22,18 +24,23 @@ var validateEmail = function(_email) {
 var validatePassword1 = function(str) {
 	var pw1 = document.getElementById(str).value;
 	if (pw1.length < 6) {
+		$('#password1Error').empty();
 		$('#password1Error').append('<p>Sorry, that password is too short</p>');
 		return false; 
 	} else if (pw1.length > 50) {
+		$('#password1Error').empty();
 		$('#password1Error').append('<p>Sorry, that password is too long</p>');
 		return false; 
 	} else if (pw1.search(/\d/) == -1) {
+		$('#password1Error').empty();
 		$('#password1Error').append('<p>Please include a number in your password</p>');
 		return false; 
 	} else if (pw1.search(/[a-zA-Z]/) == -1) {
+		$('#password1Error').empty();
 		$('#password1Error').append('<p>Please include some letters in your password</p>');
 		return false; 
 	} else if (pw1.search(/[^a-zA-Z0-9\!\@\#\$\%\^\&\*\(\)\_\+]/) != -1) {
+		$('#password1Error').empty();
 		$('#password1Error').append("<p>Woops, we don't allow that character</p>");
 		return false; 
 	}
@@ -44,9 +51,30 @@ var validatePassword2 = function(str) {
 	var pw1 = document.getElementById("password1").value;
 	var pw2 = document.getElementById(str).value;
 	if(pw2 !== pw1) {
+		$('#password2Error').empty();
 		$('#password2Error').append("<p>Those passwords don't match!</p>");
 		return false; 
 	} else {
 		return true;
 	}
 };
+
+
+var validateForm = function() {
+	var error = 0;
+	if(!validateUsername('username')) {
+		error++;
+	}
+	if(!validateEmail('email')) {
+    	error++;
+	}
+	if(!validatePassword1("password1")) {
+		error++;
+	}
+	if(!validatePassword2("password2")) {
+		error++;
+	}
+	if(error > 0) {
+		return false;
+	}
+}
